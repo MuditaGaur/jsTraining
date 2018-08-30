69ac7710738ee825d3115f4347bb86e54e41ccd7
MuditaGaur <41996644+MuditaGaur@users.noreply.github.com>
2018-08-30 17:20:04 +0530

    Add files via upload

---
+++ googltest.js
@@ -0,0 +1,7 @@
+
+var assert = require('assert');
+describe('https://www.google.com', function(err, body, res) {
+    it('should return statuscode when url is correct', function() {
+      assert.equal(response.statuscode, statuscode);
+    });
+  });
