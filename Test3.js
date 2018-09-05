f4c61da4d5adfa5f437238e0ed3597ff055e2d9b
MuditaGaur <41996644+MuditaGaur@users.noreply.github.com>
2018-09-05 14:14:19 +0530

    Update Test3.js

--- Test3.js
+++ Test3.js
@@ -1,31 +1,33 @@
 var request = require('request');
 var assert = require('chai').assert;
+var statuscode = 200;
+var username = 'Samantha';
 
 before(function(done) {
-  request('https://jsonplaceholder.typicode.com/users', function(err, res, body) {  
-    var statuscode = res.statuscode;
-    var username = body;
-    done();
-  })
+	request('https://jsonplaceholder.typicode.com/users', function(err, res, body) {  
+		var statuscode = res.statuscode;
+		var username = res.body;
+		done();
+	})
 })
 	
-	
 	describe('open url', function() {
-	  it('respond with opening url', function() {
-	  assert.equals(statuscode,200,'Status not matching');
+		it('respond with opening url', function() {
+			assert.equal(statuscode,200,'Status not matching');
+			console.log(statuscode);
+		})
 	})
-      })
 	
 	
 	describe('find user', function(done) {
-          it('respond with usernname', function() {
+		it('respond with usernname', function(body) {
+		  for(i=0; i<username.length; i++) {
+			 if(username[i].username == 'Samantha'){
+				console.log(username[i].email);
+					}					
 			
-	   for(i=0; i<username.length; i++) {
-	     if(username[i].username == 'samantha'){
-	       console.log(username[i].email);
-	       }					
-	    }
-          });
+				}
+		});
 	});
 	
 
