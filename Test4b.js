cb1a6f00d74bd21237cffc0ec431482d80805b5f
MuditaGaur <41996644+MuditaGaur@users.noreply.github.com>
2018-09-10 12:14:00 +0530

    Add files via upload

---
+++ Test4b.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var fs = require('fs');
+var assert = require('assert');
+var Promise = require('es6-promise').Promise;
+
+var myRequestFunc = function() {
+	target = 'https://www.google.com';
+	var get = function(url) {
+		return new promise(function(resolve, reject) {
+			function reqCallback(err, res, body) {
+			if (err) reject(err);
+			else resolve(body);
+			};
+			request(url, reqCallback);
+		});
+	};
+	get(target).then(function(res) {
+		result = res;
+		consolr.log(res);
+	});
+	return result;
+};
+
+var myfunc = myRequestFunc();
+console.log(myfunc);
