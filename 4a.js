88b8e92b57fd54d9e7c5b107e1c76063a4ef43e4
MuditaGaur <41996644+MuditaGaur@users.noreply.github.com>
2018-09-10 15:12:57 +0530

    Update 4a.js

--- 4a.js
+++ 4a.js
@@ -1,37 +1,34 @@
 var assert = require('assert'); 
 var fs = require('fs');
 var request = require('request');
-var url1 = require('url');
-var url2 = require('url');
-{
+
+var url1 =  'https://www.google.com';
+var url2 = 'https://www.gmail.com';
+
+
 fs.writeFile('url', function myRequestFunc(url1, url2, cb) {
-	url1 = 'https://www.google.com';
-	url2 = 'https://www.gmail.com';
-})
-request('https://www.google.com', function(err, res) {
+
+request('url1', function(err, res) {
   if (err) { 
-  return callback(err); 
+  return err; 
   }
   callback(res.url1);
-  response1 = callback.url1;
+  callback1 = callback.url1;
   })
 
-request('https://www.gmail.com', function(err, res) {
+request('url2', function(err, res) {
   if (err) { 
-  return callback(err); 
+  return err; 
   }
   callback(res.url2);
-  response2 = callback.url2;
+  callback2 = callback.url2;
   })
+})
 
-var callback = function(response1, response2, err) {
-	assert.equal(response1, response2, 'these 2 are different urls');
+var callback = function(callback1,  callback2, err) {
+	assert.equal(callback1,  callback2, 'these 2 are different urls');
+	console.log('different urls');
 	}
-
-console.log('different urls');
-}
-
-
 
 
 
